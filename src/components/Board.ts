ea3b82e965458fda361aaebc0f34b90d7431a58d
tomadanelia <tomadanelia16@gmail.com>
2025-10-27 14:52:25 +0400

    main draw function for board

--- src/components/Board.ts
+++ src/components/Board.ts
@@ -2,15 +2,41 @@ import * as PIXI from 'pixi.js';
 import type { BoardSquare } from '../types/gameTypes';
 export default class Board extends PIXI.Container {
     private layout: BoardSquare[];
+
     private squares: PIXI.Graphics[] = [];
+
+    private SQUARE_SIZE=80;
+    
+    private SQUARE_PADDING=10;
+
+
     constructor(layout:BoardSquare[]) {
         super();
         this.layout = layout;
+        this.renderSquares();
     }
+
+
     getLayout():BoardSquare[] {
         return this.layout;
     }
+
+
     getSquares():PIXI.Graphics[] {
         return this.squares;
+    }
+
+
+    renderSquares() :void{
+    this.layout.forEach((square,index)=>{
+        const squareObject=new PIXI.Graphics();
+        let rect=squareObject.rect(0,0,this.SQUARE_SIZE,this.SQUARE_SIZE);
+        square.isBonus? rect.fill(0x4CAF50):rect.fill(0x2196F3);
+        rect.stroke({width:2,color:0xffffff});
+        squareObject.x=square.position.x;
+        squareObject.y=square.position.y;
+        this.squares.push(squareObject);
+        this.addChild(squareObject);
+    })
     }
 }   
